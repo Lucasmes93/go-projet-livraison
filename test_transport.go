7683afa694c8bb4070240beec5f7ed9bfb19f948
masterivanic <masterivanic@gmail.com>
2025-03-11 17:40:48 +0100

    feat: add channel feature

--- test_transport.go
+++ test_transport.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"gestion_livraison/transports" // Remplace "gestion_livraison" par le bon nom de ton module
 	"math/rand"
+	"testing"
 	"time"
 )
 
@@ -77,9 +78,51 @@ func (t TestTransport) GetStatus() string {
 	return "Test OK"
 }
 
+func TestTruckDelivery(t *testing.T) {
+    truck := transports.Truck{ID: "T123", Capacity: 10}
+    _, err := truck.DeliverPackage("Paris")
+    if err != nil {
+        t.Errorf("Truck failed to deliver")
+    }
+}
+
+func TestDroneDelivery(t *testing.T) {
+    drone := transports.Drone{ID: "D456", Battery: 100}
+    _, err := drone.DeliverPackage("New York")
+    if err != nil {
+        t.Errorf("Drone failed to deliver")
+    }
+}
+func TestBoatDelivery(t *testing.T) {
+    boat := transports.Boat{ID: "B789", Weather: "Clear"}
+    _, err := boat.DeliverPackage("London")
+    if err != nil {
+        t.Errorf("Boat failed to deliver")
+    }
+}
+
+func TrackDelivery(transport transports.TransportMethod, destination string, ch chan string) {
+	status, err := transport.DeliverPackage(destination)
+	if err != nil {
+		ch <- fmt.Sprintf("Delivery failed: %v", err)
+		return
+	}
+	ch <- status
+}
+
 func main() {
 	// Création d'un objet qui respecte TransportMethod
 	var transport transports.TransportMethod = TestTransport{ID: "TEST123"}
+
+	ch := make(chan string)
+
+	truck, _ := transports.GetTransportMethod("truck")
+	drone, _ := transports.GetTransportMethod("drone")
+	boat, _ := transports.GetTransportMethod("boat")
+
+	go TrackDelivery(truck, "New York", ch)
+	go TrackDelivery(drone, "Los angeles", ch)
+	go TrackDelivery(boat, "Paris", ch)
 
 	// Test des méthodes
 	result, err := transport.DeliverPackage("Paris")
@@ -90,4 +133,10 @@ func main() {
 	}
 
 	fmt.Println("État :", transport.GetStatus())
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(<-ch)
+	}
+
+	
 }
